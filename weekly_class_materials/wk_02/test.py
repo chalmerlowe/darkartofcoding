233d48ca130c8edd87937c030a9bd81a351b8b7f
chalmerlowe <chalmer.lowe@gmail.com>
2015-09-02 22:28:30 -1000

    added changes to chap 2 and 4

---
+++ weekly_class_materials/wk_02/test.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+get_ipython().system('ls')
+get_ipython().magic('quickref')
+listdir = get_ipython().getoutput('ls')
---
+++ weekly_class_materials/wk_04/chapter.04.py
@@ -0,0 +1,71 @@
+
+# coding: utf-8
+
+# <h1>Welcome to the Dark Art of Coding:</h1>
+# <h2>Introduction to Python</h2>
+# Chapter 04: Lists
+
+# <h1>Today's class</h1>
+# <ul>
+# <li><strong>Review and Questions?</strong></li>
+# <li><strong>Objectives:</strong></li>
+# <ul>
+#     <li><strong>What is a list?</strong></li>
+#     <li><strong>List indexes</strong></li>
+#     <li><strong>Using lists</strong></li>
+#     <li><strong>Augmented Assignment</strong></li>
+#     <li><strong>Using and understanding methods</strong></li>
+# </ul>
+# <li><strong>In-class Practice</strong></li>
+# </ul>
+
+# <h1>Review and Questions?</h1>
+# <ul>
+# <li><strong>Functions</strong></li>
+#     <li><strong>Defining</strong></li>
+#     <li><strong>Passing Arguments</strong></li>
+#     <li><strong>Returning values</strong></li>
+#     <li><strong>Keyword arguments</strong></li>
+#     <li><strong>Global and Local scope</strong></li>
+# </ul>
+
+# <h1>What is a list?</h1>
+
+# In[2]:
+
+heroes = ['Batman', 'Green lantern', 'Aquaman', 'Flash']
+print(heroes)
+
+
+# In[5]:
+
+print(heroes[0])
+print(heroes[1])
+print(heroes[2])
+print(heroes[3])
+
+
+# In[7]:
+
+print('The ' + heroes[0] + ' was totally way cooler than ' + heroes[2])
+
+
+# In[10]:
+
+heroes[1.0]
+
+
+# In[11]:
+
+heroes[int(1.0)]
+
+
+# In[12]:
+
+heroes[42]
+
+
+# In[ ]:
+
+
+
